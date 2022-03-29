9f45cef6ed66e127f99cae4199c3dec3be18aee7
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-03-29 15:25:15 +0530

    settings update

---
+++ src/app/controllers/SettingController.php
@@ -0,0 +1,44 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+
+class SettingController extends Controller
+{
+    public function indexAction()
+    {
+        $this->view->setting = Settings::findFirst();
+        if ($_POST) {
+            $escaper = new \App\components\MyEscaper();
+            $op = $escaper->sanitize($this->request->getPost('title'));
+            $price = $escaper->sanitize($this->request->getPost('price'));
+            $stock = $escaper->sanitize($this->request->getPost('stock'));
+            $zip = $escaper->sanitize($this->request->getPost('zipcode'));
+            $setting = Settings::findFirst();
+            try {
+                $setting->assign(
+                    array(
+                        'title_optimization' => $op,
+                        'price' => $price,
+                        'zipcode' => $zip,
+                        'stock' => $stock,
+                    ),
+                    [
+                        'title_optimization',
+                        'price',
+                        'zipcode',
+                        'stock',
+                    ]
+                );
+                if ($setting->save()) {
+                    $this->flash->success("Settings updated successFully");
+                    return;
+                } else {
+                    $this->flash->error("One or More field is empty.");
+                }
+            } catch (Exception $e) {
+                $this->flash->error($e);
+            }
+        }
+    }
+}
---
+++ src/app/models/Settings.php
@@ -0,0 +1,12 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+class Settings extends Model
+{
+    public $id;
+    public $title_optimization;
+    public $price;
+    public $stock;
+    public $zipcode;
+}
